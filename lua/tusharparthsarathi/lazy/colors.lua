99e9c8be353fa7e3d2b1acc5c815385d54c86c2f
Tushar Kumar <tusharparthsarathi@Tushars-MacBook-Pro.local>
2025-05-14 20:05:51 +0530

    setup done

---
+++ lua/tusharparthsarathi/lazy/colors.lua
@@ -0,0 +1,37 @@
+function ColorMyPencils(color)
+    color = color or "rose-pine-moon"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return {
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+            })
+
+            ColorMyPencils();
+        end
+    },
+    {
+        'projekt0n/github-nvim-theme',
+        name = 'github-theme',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('github-theme').setup({
+                -- ...
+            })
+
+            ColorMyPencils();
+        end,
+    }
+}
